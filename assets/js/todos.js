8ec6494d7641fc546490a58448314495c1c7b354
Missael Romero <missael7994@gmail.com>
2017-03-30 13:26:11 -0700

    js added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,16 @@
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('completed');
+});
 
+$('ul').on('click', 'span', function(event) {
+    $(this).parent().remove();
+    event.stopPropagation();
+});
+
+$('input[type= "text"]').keypress(function(e) {
+    if (e.which === 13) {
+        var todoText = $(this).val();
+        $(this).val('');
+        $('ul').append("<li><span>X</span> " + todoText + "</li>")
+    }
+});
